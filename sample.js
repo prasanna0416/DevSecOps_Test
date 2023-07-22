43b20236317d146c31bef0fc8cea5ffaee3dfe2d
prasanna0416 <130251514+prasanna0416@users.noreply.github.com>
2023-07-22 21:01:15 +0100

    Update and rename sample.js to sample.txt

--- sample.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-function insecureHash(password) {
-  return crypto.createHash('md5').update(password).digest('hex');
-}
-
-const userInputPassword = 'secretPassword123';
-const hashedPassword = insecureHash(userInputPassword);
-console.log('Hashed password:', hashedPassword);
